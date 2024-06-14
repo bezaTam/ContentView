03a32cb9c646c3c998851ed56d738ffee3c012bc
Beza Tameru <bbt0002@auburn.edu>
2024-06-14 14:42:31 -0500

    Initial Commit

---
+++ ContentView/ContentViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentViewApp.swift
+//  ContentView
+//
+//  Created by Bezawit Tameru on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ContentViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
